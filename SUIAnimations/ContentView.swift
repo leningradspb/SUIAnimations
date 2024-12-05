e2e88070001f61b5fb5ac16aea79c8bebec5659f
Ed <Kanevsky>
2024-12-05 19:43:20 +0300

    Initial Commit

---
+++ SUIAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUIAnimations
+//
+//  Created by Eduard Kanevskii on 05.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUIAnimations/SUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIAnimationsApp.swift
+//  SUIAnimations
+//
+//  Created by Eduard Kanevskii on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
